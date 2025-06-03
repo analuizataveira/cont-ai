1da7768eededff71cb7e59855066e0f59e2966d5
Ana Luiza <106170960+analuizataveira@users.noreply.github.com>
2025-06-03 18:29:32 -0300

    fix :hammer: pipeline

--- backend/cont-ai/jest.config.js
+++ backend/cont-ai/jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts']
+  testMatch: ['**/tests/**/*.test.ts'],
+  reporters: [
+    "default",
+    ["jest-html-reporters", {
+      publicPath: "./src/test-report",
+      filename: "report.html",
+      openReport: false,
+    }]
+  ]
 };
