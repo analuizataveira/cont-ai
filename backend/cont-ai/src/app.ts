80978e99c68ef912edbb4d1594d29db36713bebd
Ana Luiza <106170960+analuizataveira@users.noreply.github.com>
2025-06-04 16:54:59 -0300

    fix :hammer: CORS configuration

--- backend/cont-ai/src/app.ts
+++ backend/cont-ai/src/app.ts
@@ -6,8 +6,13 @@ import recordRoutes from "./routes/recordRoutes";
 import userRoutes from "./routes/userRoutes"; 
 
 const app = express();
+
+// CORS configuration
+app.use(cors({
+  origin: ['https://cont-ai-eight.vercel.app', 'httsp://localhost:5173']
+}));
+
 app.use(bodyParser.json());
-app.use(cors());
 app.use(express.json());
 
 app.use("/api", recordRoutes); 
