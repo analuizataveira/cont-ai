273ddf2f5817cd2ba95d5601c5e2bac122de5b57
Ana Luiza <106170960+analuizataveira@users.noreply.github.com>
2025-06-04 20:51:52 -0300

    fix :hammer: pipeline

--- backend/cont-ai/src/database/data-source.ts
+++ backend/cont-ai/src/database/data-source.ts
@@ -3,18 +3,22 @@ import { DataSource } from "typeorm";
 import { Record } from "../entities/Records";
 import { User } from "../entities/User";
 import { config } from "dotenv";
-config();
 
-// This file sets up the TypeORM data source for the application, connecting to a PostgreSQL database.
+// Carrega .env específico conforme NODE_ENV
+const envFile = process.env.NODE_ENV === "test" ? ".env.test" : ".env";
+config({ path: envFile });
+
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST ?? "localhost",
-  port: parseInt(process.env.DB_PORT ?? "5432"),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME ,
-  synchronize: true, // Automatically creates/updates database tables to match entities
+  host: process.env.DB_HOST ?? process.env.DB_TEST_HOST ?? "localhost",
+  port: parseInt(process.env.DB_PORT ?? process.env.DB_TEST_PORT ?? "5432"),
+  username: process.env.DB_USERNAME ?? process.env.DB_TEST_USERNAME,
+  password: process.env.DB_PASSWORD ?? process.env.DB_TEST_PASSWORD,
+  database: process.env.DB_NAME ?? process.env.DB_TEST_NAME,
+  synchronize: true, // Automatically creates/updates database tables 
   logging: false, // Disables SQL query logging
-  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
-  entities: [Record, User], // Database tables
+  ssl: (process.env.DB_SSL === "true" || process.env.DB_TEST_SSL === "true") 
+    ? { rejectUnauthorized: false } 
+    : false,
+  entities: [Record, User],
 });
--- backend/cont-ai/src/server.ts
+++ backend/cont-ai/src/server.ts
@@ -1,11 +1,19 @@
+import dotenv from "dotenv";
 import { AppDataSource } from "./database/data-source";
 import "reflect-metadata";
 import app from "./app";
 
+
+if (process.env.NODE_ENV === "test") {
+  dotenv.config({ path: ".env.test" });
+} else {
+  dotenv.config();
+}
+
 AppDataSource.initialize()
-    .then(() => {
-        app.listen(3000, () => {
-            console.log("Server running on http://localhost:3000");
-        });
-    })
-    .catch((err) => console.error("Database connection failed", err));
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running on http://localhost:3000");
+    });
+  })
+  .catch((err) => console.error("Database connection failed", err));
